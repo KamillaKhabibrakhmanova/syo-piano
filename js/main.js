c8c2345c7537bdff89779859fca165ed0696654e
sgtextreme <theildar@gmail.com>
2015-10-16 10:21:22 -0400

    Contact Scroll

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ $('a[href*=#testimonials]').on('click', function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 });
+$('a[href*=#contact]').on('click', function(event){     
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});33
